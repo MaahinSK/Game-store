6ec471b2177bb5d27c961f8aa2653575541e8282
MaahinSK <maahin810@gmail.com>
2025-10-08 21:38:38 +0600

    error page fix

--- src/components/AppNotFound.jsx
+++ src/components/AppNotFound.jsx
@@ -5,7 +5,7 @@ export default function AppNotFound() {
   const nav = useNavigate();
   return (
     <div className="max-w-3xl mx-auto px-4 py-20 text-center">
-      <img src="/public/App-Error.png" alt="app not found" className="mx-auto w-114" />
+      <img src="/App-Error.png" alt="app not found" className="mx-auto w-114" />
       <h2 className="text-3xl font-bold mt-6">App not found</h2>
       <p className="text-slate-600 mt-2 text-xl">The app you tried to access was not found.</p>
       <div className="mt-6">
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   const nav = useNavigate();
   return (
     <div className="max-w-3xl mx-auto px-4 py-20 text-center">
-      <img src="/public/error-404.png" alt="not found" className="mx-auto w-115" />
+      <img src="/error-404.png" alt="not found" className="mx-auto w-115" />
       <h2 className="text-3xl font-bold mt-6">Page not found</h2>
       <p className="text-slate-600 mt-2 text-xl">The page you are looking for does not exist.</p>
       <div className="mt-6">
